5cae5bff252491c7216671bd3740ac3556addd3d
beksultankarimov <74670142+beksultankarimov@users.noreply.github.com>
2021-01-07 21:34:30 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import xlrd
-import altair as alt
+from pygeocoder import Geocoder
 
 
 #connecting to the database
@@ -41,11 +41,6 @@ depth_min = database_dfs.Depthkm.min()
 depth_max = database_dfs.Depthkm.max() 
 depth_mode = database_dfs.Depthkm.mode()[0]
 
-regions = ['All']
-regions_list = (database_dfs.Region_name.dropna().unique())
-for i in np.unique(regions_list):
-    regions.append(i)
-regions = tuple(regions)
 
 st.write("""
 # Worldwide Earthquake Monitor
@@ -66,8 +61,6 @@ def user_input_features():
     magnitude = st.sidebar.selectbox('Magnitude', ('All', 1,2,3,4,5,6,7,'8+'))
     start_date = st.sidebar.date_input('Start Date',least_recent_date)
     end_date = st.sidebar.date_input('End Date',recent_date)
-    
-
     if start_date <= end_date:
         st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
     else:
@@ -82,8 +75,8 @@ def user_input_features():
 
 
 
-st.subheader('Filter result')
 
+st.subheader('Filter result')
 input_data = user_input_features()
 if input_data['mag_type']!='All':
     result_df = database_dfs.loc[database_dfs.Mag == input_data['mag_type']]
@@ -106,64 +99,15 @@ result_df = result_df.loc[(result_df.Date  >= input_data['start_date']) & (resul
 
 # Map
 st.write("Eearthquakes happeed between: ", input_data['start_date']," and ",input_data['end_date'])
+#midpoint = (np.average(result_df.latitude), np.average(result_df.longitude))
 st.map(result_df)
 
-#Timeseries
-if st.checkbox("Show Timeseries data", False):
-    crostab_by_date = pd.crosstab(index = result_df['Date'], columns='Count').reset_index()#.sort_values(by='Date', ascending = False)
-    count = crostab_by_date.Count
-    crostab_by_date.Date = pd.to_datetime(crostab_by_date.Date)
-    
-    chart = (alt.Chart(crostab_by_date)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="Date",
-                y='Count'                
-                )
-            )
-    st.altair_chart(chart, use_container_width=True)
-
-
-
 #Raw data display
-if st.checkbox("Show Raw Data", False):
+if st.sidebar.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
     st.write("Number of Earthquakes: %i" % result_df.shape[0])
     st.write(result_df.drop(['Upload_time', 'Date'], axis=1).sort_values(by='Date_TimeUTC'))
 
+#region = st.sidebar.selectbox('Region', result_df)
+#By region
 
-if st.sidebar.checkbox("Show Data By Region", False):
-    region = st.sidebar.multiselect('Select a region', regions)
-    if 'All' not in region:
-        result_df_region = result_df.drop(['Upload_time'], axis=1)
-        result_df_region = result_df_region.loc[result_df_region.Region_name.isin(region)]
-        st.write("Number of Earthquakes in chosen region(s) %i" % result_df_region.shape[0])
-        st.map(result_df_region)
-
-        regions_df = {}
-        regions_df = {elem : pd.DataFrame() for elem in region}
-        for i in regions_df:
-            regions_df[i] = pd.DataFrame(result_df_region.loc[result_df_region.Region_name == i])
-        for i in regions_df:
-            crostab_by_date_region = pd.crosstab(index = regions_df[i]['Date'], columns='Count').reset_index()
-            count = crostab_by_date_region.Count
-            crostab_by_date_region.Date = pd.to_datetime(crostab_by_date_region.Date)
-            
-
-            chart = (alt.Chart(crostab_by_date_region)
-                    .mark_area(opacity=0.3)
-                    .encode(
-                        x="Date",
-                        y='Count',
-                        
-                        )
-                    )
-            st.write(i)
-            st.write(crostab_by_date_region)
-            st.altair_chart(chart, use_container_width=True)
-    else:
-        result_df_region = result_df
-        st.write("Number of Earthquakes in chosen region(s) %i" % result_df_region.shape[0])
-        st.map(result_df_region)
-    if st.checkbox("Show Raw Data By Region", False):
-        st.write(result_df_region.sort_values(by= 'Date_TimeUTC'))
